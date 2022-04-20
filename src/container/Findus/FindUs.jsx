d9a449c1c19bd75c6a7e34a87f0377feda3180fb
Tymoteusz Karczewski <tymkar2020@gmail.com>
2022-04-20 11:45:58 +0200

    Font sizes and gallery bug fix

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-import { images } from "../../constants";
 import { SubHeading } from "../../components";
+import { images } from "../../constants";
 
 const FindUs = () => (
-  <div className="app__bg app__wrapper section padding" id="contact">
+  <div className="app__bg app__wrapper section__padding" id="contact">
     <div className="app__wrapper_info">
       <SubHeading title="Contact" />
       <h1 className="headtext__cormorant" style={{ marginBottom: "3rem" }}>
@@ -12,24 +12,28 @@ const FindUs = () => (
       </h1>
       <div className="app__wrapper-content">
         <p className="p__opensans">
-          9071 Golden Star Street West Chester, PA 19380
+          Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 75G
         </p>
         <p
           className="p__cormorant"
-          style={{ color: "#dcca87", margin: "2rem 0" }}
+          style={{ color: "#DCCA87", margin: "2rem 0" }}
         >
           Opening Hours
         </p>
-        <p className="p__cormorant">Mon - Fri: 10:00 am - 2:00 pm</p>
-        <p className="p__cormorant">Sat - Sun: 10:00 am - 2:00 pm</p>
+        <p className="p__opensans">Mon - Fri: 10:00 am - 02:00 am</p>
+        <p className="p__opensans">Sat - Sun: 10:00 am - 03:00 am</p>
       </div>
-
-      <button className="custom__button" style={{ marginTop: "2rem" }}>
+      <button
+        type="button"
+        className="custom__button"
+        style={{ marginTop: "2rem" }}
+      >
         Visit Us
       </button>
     </div>
+
     <div className="app__wrapper_img">
-      <img src={images.findus} alt="find us" />
+      <img src={images.findus} alt="finus_img" />
     </div>
   </div>
 );
