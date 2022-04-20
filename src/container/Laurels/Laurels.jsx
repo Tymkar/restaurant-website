39c5ef6abc9b6b99e9bcdde72cb99e4f5c192ec1
Tymoteusz Karczewski <tymkar2020@gmail.com>
2022-04-20 11:26:06 +0200

    Laurels bug fix and media query

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { images, data } from "../../constants";
-import { SubHeading } from "../../components";
 
+import { SubHeading } from "../../components";
+import { images, data } from "../../constants";
 import "./Laurels.css";
 
 const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
   <div className="app__laurels_awards-card">
-    <img src={imgUrl} alt="award" />
+    <img src={imgUrl} alt="awards" />
     <div className="app__laurels_awards-card_content">
-      <p className="p__cormorant" style={{ color: "dcca87" }}>
+      <p className="p__cormorant" style={{ color: "#DCCA87" }}>
         {title}
       </p>
-      <p className="p__cormorant">{subtitle}</p>
+      <p className="p__opensans">{subtitle}</p>
     </div>
   </div>
 );
@@ -19,16 +19,18 @@ const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
 const Laurels = () => (
   <div className="app__bg app__wrapper section__padding" id="awards">
     <div className="app__wrapper_info">
-      <SubHeading title="Awards & Recognition" />
+      <SubHeading title="Awards & recognition" />
       <h1 className="headtext__cormorant">Our Laurels</h1>
+
       <div className="app__laurels_awards">
         {data.awards.map((award) => (
           <AwardCard award={award} key={award.title} />
         ))}
       </div>
     </div>
+
     <div className="app__wrapper_img">
-      <img src={images.laurels} alt="laurels" />
+      <img src={images.laurels} alt="laurels_img" className="laurels__img" />
     </div>
   </div>
 );
