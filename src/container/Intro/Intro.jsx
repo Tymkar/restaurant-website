eba20c40b071e00e9311b086319f81751e7c140b
Tymoteusz Karczewski <tymkar2020@gmail.com>
2022-04-20 11:15:28 +0200

    Intro media query

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,29 +1,18 @@
 import React from "react";
-import { useState } from "react";
 import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
-import { meal } from "../../constants";
 
+import { meal } from "../../constants";
 import "./Intro.css";
 
 const Intro = () => {
-  const [playVideo, setPlayVideo] = useState(false);
+  const [playVideo, setPlayVideo] = React.useState(false);
   const vidRef = React.useRef();
-
-  const handleVideo = () => {
-    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
-
-    if (playVideo) {
-      vidRef.current.pause();
-    } else {
-      vidRef.current.play();
-    }
-  };
 
   return (
     <div className="app__video">
       <video
-        src={meal}
         ref={vidRef}
+        src={meal}
         type="video/mp4"
         loop
         controls={false}
@@ -32,7 +21,14 @@ const Intro = () => {
       <div className="app__video-overlay flex__center">
         <div
           className="app__video-overlay_circle flex__center"
-          onClick={handleVideo}
+          onClick={() => {
+            setPlayVideo(!playVideo);
+            if (playVideo) {
+              vidRef.current.pause();
+            } else {
+              vidRef.current.play();
+            }
+          }}
         >
           {playVideo ? (
             <BsPauseFill color="#fff" fontSize={30} />
